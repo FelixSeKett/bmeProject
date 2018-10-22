fe702f0c0f93c9a2a6ee4c2ed767d1d3aca857ed
Felix@Nightingale <mail2@felixbaumgarten.de>
2018-10-22 09:13:40 +0200

    Test

---
+++ desktop/src/com/bmeproject/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bmeproject.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bmeproject.game.BMEProject;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BMEProject(), config);
+	}
+}
