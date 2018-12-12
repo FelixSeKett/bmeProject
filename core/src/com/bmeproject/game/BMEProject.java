96962726a86c8d25e158825c8eb8331736c2641e
Gabriel <gabriel.veiz@gmx.de>
2018-12-12 17:34:56 +0100

    Multiplayer Demo

--- core/src/com/bmeproject/game/BMEProject.java
+++ core/src/com/bmeproject/game/BMEProject.java
@@ -1,9 +1,16 @@
 package com.bmeproject.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.bmeproject.game.bmeProject.*;
 
 import java.util.HashMap;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Die Wurzel des Code-Stamms und Erweiterung von {@link Game}. Wird das Spiel gestartet, erstellt die jeweilige
@@ -24,7 +31,7 @@ public class BMEProject extends Game
 	private TestScreen              testScreen;
 	private Profile                 profile;
 	private HashMap<String, Entity> entities;
-
+	private Socket socket;
 	// ===================================
 	// PROCEDURES
 	// ===================================
@@ -40,37 +47,76 @@ public class BMEProject extends Game
 		deckScreen = new DeckScreen(this);
 		testScreen = new TestScreen(this);
 		profile = new Profile(this);
-		entities = new HashMap<>();
+		entities = new HashMap();
+		connectSocket();
+		configSocketEvents();
 
 		setScreen(titleScreen);
 	}
 
-	public void activateTitleScreen()
-	{
-		setScreen(titleScreen);
+	public void connectSocket(){
+		try {
+			socket = IO.socket("http://localhost:8080");
+			socket.connect();
+		} catch(Exception e) {
+			System.out.println(e);
+		}
 	}
 
-	public void activateBattleScreen()
-	{
-		setScreen(battleScreen);
-	}
+	public void configSocketEvents () {
+		socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+			@Override
+			public void call(Object... args) {
+				Gdx.app.log("SocketIO", "Connected");
+			}
+		}).on("socketID", new Emitter.Listener() {
+			@Override
+			public void call(Object... args) {
+				JSONObject data = (JSONObject) args[0];
+				try {
+					String id = data.getString("id");
+					Gdx.app.log("SocketIO", "My ID: " + id);
 
-	public void activateDeckScreen()
-	{
-		setScreen(deckScreen);
-	}
+				} catch (JSONException e) {
+					Gdx.app.log("SocketIO", "Error getting ID");
+				}
+			}
+		}).on("newPlayer", new Emitter.Listener() {
+			public void call(Object... args) {
+				JSONObject data = (JSONObject) args[0];
+				try {
+					String id = data.getString("id");
+					Gdx.app.log("SocketIO", "New Player Connected: " + id);
+				} catch (JSONException e) {
+					Gdx.app.log("SocketIO", "Error getting New Player ID");
+				}
+			}
+		});
+		}
 
-	public void activateTestScreen()
-	{
-		setScreen(testScreen);
-	}
 
-	// ===================================
-	// FUNCTIONS
-	// ===================================
+			public void activateTitleScreen() {
+				setScreen(titleScreen);
+			}
 
-	public Profile getProfile()
-	{
-		return profile;
-	}
+			public void activateBattleScreen() {
+				setScreen(battleScreen);
+			}
+
+			public void activateDeckScreen() {
+				setScreen(deckScreen);
+			}
+
+			public void activateTestScreen() {
+				setScreen(testScreen);
+			}
+
+			// ===================================
+			// FUNCTIONS
+			// ===================================
+
+			public Profile getProfile() {
+				return profile;
+			}
 }
+
