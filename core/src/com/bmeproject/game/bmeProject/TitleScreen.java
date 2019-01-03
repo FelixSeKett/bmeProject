eb3c721e5fb09faccc4408db0f31f644f8135f01
Manuela M <mosandlma69267@th-nuernberg.de>
2019-01-03 19:30:07 +0100

    #23_titlescreen
    Created our first button with Cigdem

--- core/src/com/bmeproject/game/bmeProject/TitleScreen.java
+++ core/src/com/bmeproject/game/bmeProject/TitleScreen.java
@@ -1,6 +1,13 @@
 package com.bmeproject.game.bmeProject;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.bmeproject.game.BMEProject;
+
+import javax.swing.*;
 
 public class TitleScreen extends TheatricalScreen
 {
@@ -17,10 +24,30 @@ public class TitleScreen extends TheatricalScreen
 	// METHODS
 	// ===================================
 
+	private Stage stage;
+	private Skin skin;
+
 	@Override public void show()
 	{
-		super.show();
+		//super.show();
 		System.out.println("TITLE SCREEN SHOWN");
-		BME_PROJECT.activateTestScreen();
+		//BME_PROJECT.activateTitleScreen();
+
+		skin = new Skin (Gdx.files.internal("uiskin.json"));
+		stage = new Stage(new ScreenViewport());
+
+		final TextButton button = new TextButton("Click me", skin, "default");
+		button.setWidth(200);
+		button.setHeight(50);
+		button.setPosition(stage.getWidth()/2, stage.getHeight()/2);
+		stage.addActor(button);
+
+		Gdx.input.setInputProcessor(stage);
 	}
+
+	@Override public void render(float delta)
+	{
+		stage.draw();
+	}
+
 }
