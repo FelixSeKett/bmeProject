e0cad3ac6030052e4efc946f97dbfefbeda3823f
Cigdem <cigdem_medigc@yahoo.de>
2019-01-09 12:01:44 +0100

    23_exitButton and load new screen

--- core/src/com/bmeproject/game/bmeProject/TitleScreen.java
+++ core/src/com/bmeproject/game/bmeProject/TitleScreen.java
@@ -5,9 +5,11 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.bmeproject.game.BMEProject;
 
@@ -37,7 +39,6 @@ public class TitleScreen extends TheatricalScreen
 	@Override public void show(){
 		super.show();
 		System.out.println("TITLE SCREEN SHOWN");
-		//BME_PROJECT.activateTestScreen();
 
 		skin = new Skin(Gdx.files.internal("uiskin.json"));
 		stage = new Stage(new ScreenViewport());
@@ -51,7 +52,33 @@ public class TitleScreen extends TheatricalScreen
 		firstButton.setPosition( stage.getHeight()/2,stage.getWidth()/2);
 		firstButton.setSize(280,60);
 
+		secondButton = new TextButton("Exit", skin, "default");
+		secondButton.setPosition(stage.getHeight()/2,stage.getWidth()/2-100);
+		secondButton.setSize(280,60);
+
+		clickButton(firstButton);
+		exitButton(secondButton);
+
 		stage.addActor(firstButton);
+		stage.addActor(secondButton);
+	}
+
+	private void exitButton(TextButton btn){
+		btn.addListener(new ClickListener(){
+			@Override
+			public void clicked (InputEvent event, float x, float y){
+				Gdx.app.exit();
+			}
+		});
+	}
+
+	private void clickButton(TextButton btnClick){
+		btnClick.addListener(new ClickListener(){
+			@Override
+			public void clicked (InputEvent event, float x, float y){
+				BME_PROJECT.activateTestScreen();
+			}
+		});
 	}
 
 	@Override
