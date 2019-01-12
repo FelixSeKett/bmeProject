47a1587be24e860911c96c60f242e63349a4489f
Cigdem <cigdem_medigc@yahoo.de>
2019-01-12 19:39:16 +0100

    23-buttons and logo are added and postioned

--- core/src/com/bmeproject/game/bmeProject/TitleScreen.java
+++ core/src/com/bmeproject/game/bmeProject/TitleScreen.java
@@ -2,6 +2,7 @@ package com.bmeproject.game.bmeProject;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -10,15 +11,26 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.bmeproject.game.BMEProject;
-import com.bmeproject.game.bmeProject.entity.Background;
 
 
 public class TitleScreen extends TheatricalScreen
 {
+	private final int positionX = 16;
+	private final int postionYlogo = 300;
+	private final int positionYDeck = 4;
+
+	private final int postionYDuel = 100 ;
+	private final int postionYSettings = 200 ;
+	private final int postionXAnimation = 1000;
+
 	private Skin skin;
 	private Stage stage;
-	private TextButton firstButton;
-	private TextButton secondButton;
+	SpriteBatch batch;
+	private Texture logo;
+	private Texture buttonDeck;
+	private Texture buttonDuel;
+	private Texture buttonSettings;
+	private Texture loadAnimation;
 
 
 	// ===================================
@@ -28,6 +40,13 @@ public class TitleScreen extends TheatricalScreen
 	public TitleScreen(BMEProject bmeProject)
 	{
 		super(bmeProject);
+
+		batch = new SpriteBatch();
+		logo= new Texture("core/assets/logo.png");
+		buttonDeck = new Texture("core/assets/buttonDeck.png");
+		buttonDuel = new Texture("core/assets/buttonDuel.png");
+		buttonSettings = new Texture("core/assets/buttonSettings.png");
+		loadAnimation = new Texture("core/assets/animation.png");
 	}
 
 	// ===================================
@@ -38,7 +57,6 @@ public class TitleScreen extends TheatricalScreen
 		super.show();
 		System.out.println("TITLE SCREEN SHOWN");
 
-
 		skin = new Skin(Gdx.files.internal("uiskin.json"));
 		stage = new Stage(new ScreenViewport());
 		Gdx.input.setInputProcessor(stage);
@@ -48,22 +66,20 @@ public class TitleScreen extends TheatricalScreen
 	}
 
 
-
-
 	private void initButtons() {
-		firstButton = new TextButton("PlayOne", skin, "default");
-		firstButton.setPosition( stage.getHeight()/2,stage.getWidth()/2);
-		firstButton.setSize(280,60);
+	//	firstButton = new TextButton("PlayOne", skin, "default");
+	//	firstButton.setPosition( stage.getHeight()/2,stage.getWidth()/2);
+	//	firstButton.setSize(280,60);
 
-		secondButton = new TextButton("Exit", skin, "default");
+	/*	secondButton = new TextButton("Exit", skin, "default");
 		secondButton.setPosition(stage.getHeight()/2,stage.getWidth()/2-100);
-		secondButton.setSize(280,60);
+		secondButton.setSize(280,60);*/
 
-		clickButton(firstButton);
-		exitButton(secondButton);
+		clickButton(buttonDeck);
+	//	exitButton(buttonDuel);
 
-		stage.addActor(firstButton);
-		stage.addActor(secondButton);
+	//	stage.addActor(firstButton);
+	//	stage.addActor(secondButton);
 	}
 
 	private void exitButton(TextButton btn){
@@ -75,19 +91,44 @@ public class TitleScreen extends TheatricalScreen
 		});
 	}
 
-	private void clickButton(TextButton btnClick){
-		btnClick.addListener(new ClickListener(){
+	private void clickButton(Texture btnClick){
+		//BME_PROJECT.activateTestScreen();
+
+
+		/*btnClick.addListener(new ClickListener(){
 			@Override
 			public void clicked (InputEvent event, float x, float y){
 				BME_PROJECT.activateTestScreen();
 			}
-		});
+		});*/
 	}
 
 	@Override
 	public void render(float delta){
 		Gdx.gl.glClearColor(0x52/255.0f,0x9D/255.0f,0xBF/255.0f,0xff/255.0f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+
+		batch.begin();
+		batch.draw(logo,stage.getWidth()/positionX,stage.getHeight()-postionYlogo);
+		batch.end();
+
+		batch.begin();
+		batch.draw(buttonDeck,stage.getWidth()/positionX,stage.getHeight()/positionYDeck);
+		batch.end();
+
+		batch.begin();
+		batch.draw(buttonDuel,stage.getWidth()/positionX,stage.getHeight()/positionYDeck-postionYDuel);
+		batch.end();
+
+		batch.begin();
+		batch.draw(buttonSettings,stage.getWidth()/positionX,stage.getHeight()/positionYDeck-postionYSettings);
+		batch.end();
+
+		batch.begin();
+		batch.draw(loadAnimation,stage.getWidth()-postionXAnimation,stage.getHeight()/positionYDeck-postionYSettings,840,463);
+		batch.end();
+
 
 		stage.draw();
 	}
--- desktop/src/com/bmeproject/game/desktop/DesktopLauncher.java
+++ desktop/src/com/bmeproject/game/desktop/DesktopLauncher.java
@@ -10,5 +10,7 @@ public class DesktopLauncher
 	{
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new BMEProject(), config);
+		config.width = 1440;
+		config.height = 1024;
 	}
 }
