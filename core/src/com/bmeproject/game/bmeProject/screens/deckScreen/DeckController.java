cf4406e0c3d0102769c491f4f01e9dd6d1cec3c3
Felix@Nightingale <mail2@felixbaumgarten.de>
2019-03-27 16:34:27 +0100

    Areas implementiert, aber noch nicht getestet. Es fehlen Schnittstellen im DeckScreen / DeckController / DeckRenderer.

--- core/src/com/bmeproject/game/bmeProject/screens/deckScreen/DeckController.java
+++ core/src/com/bmeproject/game/bmeProject/screens/deckScreen/DeckController.java
@@ -1,14 +1,44 @@
 package com.bmeproject.game.bmeProject.screens.deckScreen;
 
 import com.badlogic.gdx.InputAdapter;
+import com.bmeproject.game.bmeProject.gameObjects.Card;
+import com.bmeproject.game.bmeProject.screens.Area;
 
-public class DeckController extends InputAdapter {
+import java.util.LinkedList;
 
-    public DeckController(){}
+public class DeckController extends InputAdapter
+{
+	// ===================================
+	// ATTRIBUTES
+	// ===================================
 
-    private void init(){}
+	private Area deckArea;
 
-    public void update(float DeltaTime){}
+	// ===================================
+	// CONSTRUCTORS
+	// ===================================
 
-    //initObjects + updateObjects + Controls
+	public DeckController() { }
+
+	// ===================================
+	// METHODS
+	// ===================================
+
+	private void init()
+	{
+		LinkedList<Card> testList = new LinkedList<>();
+		for (int i = 0; i < 10; i++)
+		{
+			testList.add(new Card());
+		}
+		deckArea = new Area(100f, 100f, 0f, 0f, 20f, 200f, 0f, -100f, 50f, 0, testList, 6);
+		deckArea.update();
+	}
+
+	public void update(float DeltaTime)
+	{
+		deckArea.update();
+	}
+
+	//initObjects + updateObjects + Controls
 }
