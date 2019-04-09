d2edeb17e374bcf624d39f9e61dd95de4cea0132
Cigdem <cigdem_medigc@yahoo.de>
2019-04-09 19:32:42 +0200

    #23 TestScreen wird via Klick auf ImageButton aufgerufen

--- core/src/com/bmeproject/game/bmeProject/screens/titleScreen/TitleScreen.java
+++ core/src/com/bmeproject/game/bmeProject/screens/titleScreen/TitleScreen.java
@@ -2,11 +2,16 @@ package com.bmeproject.game.bmeProject.screens.titleScreen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.bmeproject.game.BMEProject;
 import com.bmeproject.game.bmeProject.screens.AbstractScreen;
@@ -30,6 +35,8 @@ public class TitleScreen extends AbstractScreen
 	private Skin skin;
 	private Stage stage;
 	private TextButton textButton;
+	private Texture buttonTexture;
+	private Texture buttonPressTexture;
 
 	@Override public void show()
 	{
@@ -39,23 +46,42 @@ public class TitleScreen extends AbstractScreen
 		stage = new Stage(new ScreenViewport());
 		Gdx.input.setInputProcessor(stage);
 
+		buttonTexture = new Texture(Gdx.files.internal("core/assets/Button.png"));
+		buttonPressTexture = new Texture(Gdx.files.internal("core/assets/ButtononClick.png"));
+
 		initButtons();
 	}
 
 	private void initButtons() {
 		textButton = new TextButton("PlayOne", skin, "default");
-		textButton.setPosition( stage.getHeight()/2,stage.getWidth()/2);
+		textButton.setPosition( stage.getHeight()/4,stage.getWidth()/2);
 		textButton.setSize(280,60);
 
+		ImageButton imageButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(buttonTexture)),
+				new TextureRegionDrawable(new TextureRegion(buttonPressTexture)));
+		imageButton.setPosition(stage.getHeight()/4,stage.getWidth()/4);
+
+		stage.addActor(imageButton);
 		stage.addActor(textButton);
 
-		clickButton(textButton);
+		clickTextButton(textButton);
+		clickImageButton(imageButton);
 	}
 
-	private void clickButton(TextButton btnClick){
+	private void clickTextButton(TextButton btnClick){
 		btnClick.addListener(new ClickListener(){
 			@Override
 			public void clicked (InputEvent event, float x, float y){
+				BME_PROJECT.activateTestScreen();
+			}
+		});
+	}
+
+	private void clickImageButton(ImageButton btnCLick){
+		btnCLick.addListener(new ActorGestureListener() {
+			@Override
+			public void tap(InputEvent event, float x, float y, int count,
+							int button) {
 				BME_PROJECT.activateTestScreen();
 			}
 		});
