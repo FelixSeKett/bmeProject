16261f7ae75d97aedf791c6af081314f70f0ebdd
Manuela M <mosandlma69267@th-nuernberg.de>
2019-06-07 16:27:02 +0200

    Zone Class implemented

--- core/src/com/bmeproject/game/bmeProject/screens/battleScreen/Zone.java
+++ core/src/com/bmeproject/game/bmeProject/screens/battleScreen/Zone.java
@@ -1,4 +1,24 @@
 package com.bmeproject.game.bmeProject.screens.battleScreen;
 
 public enum Zone {
+
+    RED, GREEN, BLUE;
+
+    private boolean activated;
+
+    Zone() {
+        activated = false;
+    }
+
+    public boolean isActivated(){
+        return activated;
+    }
+
+    public void activate(){
+        activated = true;
+    }
+
+    public void reset() {
+        activated = false;
+    }
 }
