38ea4c273348ff811cace3d8ce582b9ccf1bddee
Manuela M <mosandlma69267@th-nuernberg.de>
2019-06-07 12:50:19 +0200

    Added Stream Class, expanded Compass
    Give direction of stream and set to opposite direction

--- core/src/com/bmeproject/game/bmeProject/screens/battleScreen/Compass.java
+++ core/src/com/bmeproject/game/bmeProject/screens/battleScreen/Compass.java
@@ -2,4 +2,18 @@ package com.bmeproject.game.bmeProject.screens.battleScreen;
 
 public class Compass {
 
+    private Stream currentStream;
+
+    public Compass(){
+        currentStream = Stream.CLOCKWISE;
+    }
+
+    private Stream getCurrentStream(){
+        return currentStream;
+    }
+
+    private void changeStreamRotation(){
+        currentStream = currentStream.giveOppositeStream();
+    }
+
 }
--- core/src/com/bmeproject/game/bmeProject/screens/battleScreen/Stream.java
+++ core/src/com/bmeproject/game/bmeProject/screens/battleScreen/Stream.java
@@ -1,4 +1,30 @@
 package com.bmeproject.game.bmeProject.screens.battleScreen;
 
 public enum Stream {
+
+    CLOCKWISE{
+        @Override public int giveFieldModifier()
+        {
+            return -1;
+        }
+        @Override public Stream giveOppositeStream()
+        {
+            return Stream.COUNTERCLOCKWISE;
+        }
+    },
+
+    COUNTERCLOCKWISE{
+        @Override public int giveFieldModifier()
+        {
+            return 1;
+        }
+        @Override public Stream giveOppositeStream()
+        {
+            return Stream.CLOCKWISE;
+        }
+    };
+
+    public abstract int giveFieldModifier();
+    public abstract Stream giveOppositeStream();
+
 }
