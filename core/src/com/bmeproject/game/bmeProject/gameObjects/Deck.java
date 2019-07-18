1f719baad08daf434cbb0c69186e068338359ade
Pia <korndoerferpi67188@th-nuernberg.de>
2019-07-18 17:26:01 +0200

    Xml befüllt Random Deck Generator erstellt (37 normale Karten und 3 basen)

--- core/src/com/bmeproject/game/bmeProject/gameObjects/Deck.java
+++ core/src/com/bmeproject/game/bmeProject/gameObjects/Deck.java
@@ -5,55 +5,58 @@ import com.bmeproject.game.BMEProject;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Deck
-{
-	private ArrayList<Integer> deck = new ArrayList<Integer>();
+public class Deck {
+    private ArrayList<Integer> deck = new ArrayList<Integer>();
 
-	public Deck()
-	{
-		//Hard gecodet
-		deck.add(1);
-		deck.add(3);
-		deck.add(4);
-		deck.add(1);
+    public Deck() {
+        addStarterCardsToDeck();
+        Collections.sort(deck);
+    }
 
-		Collections.sort(deck);
-	}
+    public ArrayList<Integer> getDeck() {
+        return deck;
+    }
 
-	public ArrayList<Integer> getDeck()
-	{
-		return deck;
-	}
+    public void addCardToDeck(int id) {
+        deck.add(id);
+    }
 
-	public void addCardToDeck(int id)
-	{
-		deck.add(id);
-	}
+    // Hinzfügen von 40 zufälligen Karten (3 Quartieren und 37 anderen Karten )in das Deck
+    public void addStarterCardsToDeck() {
+        // added 37 Karten von der Id 6-44 in das Deck (alle Karten die kein Quarier sind)
+        for (int i = 0; i < 37; i++) {
+            int random = (int) (Math.random() * 44 + 6);
+            deck.add(random);
 
-	public void removeCardFromDeck(int id)
-	{
-		deck.remove(id);
-	}
+        }
+        //added 3 Random Karten mit der Id 1-5 in das Deck (Karten die Quartiere sind)
+        for (int i = 0; i < 3; i++) {
+            int random = (int) (Math.random() * 5 + 1);
+            deck.add(random);
 
-	public int getSize()
-	{
-		return deck.size();
-	}
+        }
+    }
 
-	public int getCardIdFromDeck(int index)
-	{
-		return deck.get(index);
-	}
+    public void removeCardFromDeck(int id) {
+        deck.remove(id);
+    }
 
-	public Card getCardFromDeck(int id)
-	{
-		//hier muss noch eine Prüfung hin, ob deck überhaupt id contained
-		Card card = BMEProject.allCards.get(id);
-		return card;
-	}
+    public int getSize() {
+        return deck.size();
+    }
 
-	public ArrayList getAllCardsFromDeck(){
-		return deck;
-	}
-	// Note for ArrayLists: contains(), set(), indexOf(), clear()
+    public int getCardIdFromDeck(int index) {
+        return deck.get(index);
+    }
+
+    public Card getCardFromDeck(int id) {
+        //hier muss noch eine Prüfung hin, ob deck überhaupt id contained
+        Card card = BMEProject.allCards.get(id);
+        return card;
+    }
+
+    public ArrayList getAllCardsFromDeck() {
+        return deck;
+    }
+    // Note for ArrayLists: contains(), set(), indexOf(), clear()
 }
