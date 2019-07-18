15f6ce311b3a5db943644887ec94e05b15cf9139
Sebastian <beck_sebastian@hotmail.de>
2019-07-18 14:00:17 +0200

    Bildschirmauflösung
    - Resize Methode ausimplementiert, verzerrt nun Images nicht mehr, skaliert jedoch mit Fenstergröße mit

--- core/src/com/bmeproject/game/bmeProject/screens/Renderer.java
+++ core/src/com/bmeproject/game/bmeProject/screens/Renderer.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -29,8 +30,9 @@ public class Renderer
 	Renderer(SpriteBatch spriteBatch)
 	{
 		SPRITE_BATCH = spriteBatch;
-		camera = new PerspectiveCamera();
-		viewport = new FitViewport(800, 480, camera);
+		camera = new OrthographicCamera();
+		viewport = new FitViewport(800, 450, camera);
+		camera.position.set(400, 225, 0);
 	}
 
 	// ===================================
@@ -40,7 +42,8 @@ public class Renderer
 	void render(Stage stageToRender)
 	{
 		stage = stageToRender;
-		Gdx.gl.glClearColor(1f, 1f, 1f, 0f);
+		stage.setViewport(viewport);
+		Gdx.gl.glClearColor(0, 0, 0, 0f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		stage.draw();
 	}
@@ -48,10 +51,6 @@ public class Renderer
 	public void resize(int width, int height)
 	{
 		viewport.update(width, height);
-
-		if(stage != null){
-			stage.getViewport().update(width, height, true);
-		}
 	}
 
 	void dispose()
