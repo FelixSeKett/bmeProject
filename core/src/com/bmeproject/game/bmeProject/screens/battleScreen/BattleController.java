7bcdccc49363fca3ac3529966aa51384e178c47c
Pia <korndoerferpi67188@th-nuernberg.de>
2019-07-21 18:12:25 +0200

    Siegebedingungen Bilder

--- core/src/com/bmeproject/game/bmeProject/screens/battleScreen/BattleController.java
+++ core/src/com/bmeproject/game/bmeProject/screens/battleScreen/BattleController.java
@@ -31,182 +31,182 @@ TODO: Kosmetik
 - Buttons mit Pressed und Hovered Bildern versehen
  */
 
-public class BattleController extends Controller
-{
-	// ===================================
-	// ATTRIBUTES
-	// ===================================
+public class BattleController extends Controller {
+    // ===================================
+    // ATTRIBUTES
+    // ===================================
 
-	public final  DetailView  DETAIL_VIEW;
-	public final  ButtonView  BUTTON_VIEW;
-	public final  Battlefield BATTLEFIELD;
-	private final Player      PLAYER_1;
-	private final Player      PLAYER_2;
+    public final DetailView DETAIL_VIEW;
+    public final ButtonView BUTTON_VIEW;
+    public final Battlefield BATTLEFIELD;
+    private final Player PLAYER_1;
+    private final Player PLAYER_2;
 
-	private Player     activePlayer;
-	private boolean    started;
-	private BattleCard lastClickedBattleCard;
+    private Player activePlayer;
+    private boolean started;
+    private BattleCard lastClickedBattleCard;
 
-	// ===================================
-	// CONSTRUCTORS
-	// ===================================
+    // ===================================
+    // CONSTRUCTORS
+    // ===================================
 
-	public BattleController(SpriteBatch spriteBatch)
-	{
-		super(spriteBatch);
-		Texture background = new Texture("core/assets/visuals/spielbrettSmall.png");
-		background.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
-		Image backgroundImage = new Image(background);
+    public BattleController(SpriteBatch spriteBatch) {
+        super(spriteBatch);
+        Texture background = new Texture("core/assets/visuals/spielbrettSmall.png");
+        background.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        Image backgroundImage = new Image(background);
 
 
-		stage.addActor(backgroundImage);
-		DETAIL_VIEW = new DetailView(stage);
-		BUTTON_VIEW = new ButtonView(this);
-		BATTLEFIELD = new Battlefield(this);
-		PLAYER_1 = new Player(this, Party.ALLY);
-		PLAYER_2 = new Player(this, Party.ENEMY);
-		activePlayer = PLAYER_1;
-		activePlayer.beginTurn();
-		Gdx.input.setInputProcessor(stage);
-	}
+        stage.addActor(backgroundImage);
+        DETAIL_VIEW = new DetailView(stage);
+        BUTTON_VIEW = new ButtonView(this);
+        BATTLEFIELD = new Battlefield(this);
+        PLAYER_1 = new Player(this, Party.ALLY);
+        PLAYER_2 = new Player(this, Party.ENEMY);
+        activePlayer = PLAYER_1;
+        activePlayer.beginTurn();
+        Gdx.input.setInputProcessor(stage);
+    }
 
-	// ===================================
-	// METHODS
-	// ===================================
+    // ===================================
+    // METHODS
+    // ===================================
 
-	@Override public void update(float delta)
-	{
-		super.update(delta);
-		if (BMEProject.DEBUG) {
-			if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
-				activePlayer.drawTopCard();
-			}
-		}
-	}
+    @Override
+    public void update(float delta) {
+        super.update(delta);
+        if (BMEProject.DEBUG) {
+            if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+                activePlayer.drawTopCard();
+            }
+        }
+    }
 
-	public Player giveActivePlayer()
-	{
-		return activePlayer;
-	}
+    public Player giveActivePlayer() {
+        return activePlayer;
+    }
 
-	public BattleCard giveLastClickedBattleCard()
-	{
-		return lastClickedBattleCard;
-	}
+    public BattleCard giveLastClickedBattleCard() {
+        return lastClickedBattleCard;
+    }
 
-	public Player giveOppositePlayerOf(Player player)
-	{
-		if (player == PLAYER_1) {
-			return PLAYER_2;
-		} else {
-			return PLAYER_1;
-		}
-	}
+    public Player giveOppositePlayerOf(Player player) {
+        if (player == PLAYER_1) {
+            return PLAYER_2;
+        } else {
+            return PLAYER_1;
+        }
+    }
 
-	public Field giveCurrentFieldOfBattleCard(BattleCard battleCard)
-	{
-		Field field = null;
+    public Field giveCurrentFieldOfBattleCard(BattleCard battleCard) {
+        Field field = null;
 
-		// Checke die Fields der Sektoren des Battlefields nach der BattleCard!
-		if (BATTLEFIELD != null) {
-			field = BATTLEFIELD.giveCurrentFieldOfBattleCard(battleCard);
-		}
-		// Wenn die BattleCard auf keinem der soeben gecheckten Fields liegt, dann...
-		if (field == null) {
-			// ... checke die Fields vom ersten Spieler nach der BattleCard!
-			if (PLAYER_1 != null) {
-				field = PLAYER_1.giveCurrentFieldOfBattleCard(battleCard);
-			}
-			// Wenn die BattleCard auf keinem der soeben gecheckten Fields liegt, dann...
-			if (field == null) {
-				// Checke die Fields vom zweiten Spieler nach der BattleCard!
-				if (PLAYER_2 != null) {
-					field = PLAYER_2.giveCurrentFieldOfBattleCard(battleCard);
-				}
-			}
-		}
+        // Checke die Fields der Sektoren des Battlefields nach der BattleCard!
+        if (BATTLEFIELD != null) {
+            field = BATTLEFIELD.giveCurrentFieldOfBattleCard(battleCard);
+        }
+        // Wenn die BattleCard auf keinem der soeben gecheckten Fields liegt, dann...
+        if (field == null) {
+            // ... checke die Fields vom ersten Spieler nach der BattleCard!
+            if (PLAYER_1 != null) {
+                field = PLAYER_1.giveCurrentFieldOfBattleCard(battleCard);
+            }
+            // Wenn die BattleCard auf keinem der soeben gecheckten Fields liegt, dann...
+            if (field == null) {
+                // Checke die Fields vom zweiten Spieler nach der BattleCard!
+                if (PLAYER_2 != null) {
+                    field = PLAYER_2.giveCurrentFieldOfBattleCard(battleCard);
+                }
+            }
+        }
 
-		// Wenn die BattleCard auf keinem der soeben gecheckten Fields liegt, gib null zurück!
-		return field;
-	}
+        // Wenn die BattleCard auf keinem der soeben gecheckten Fields liegt, gib null zurück!
+        return field;
+    }
 
-	public void takeLastClickedBattleCard(BattleCard battleCard)
-	{
-		if (lastClickedBattleCard != null) {
-			lastClickedBattleCard.getUnselected();
-		}
-		battleCard.getSelected();
-		lastClickedBattleCard = battleCard;
-	}
+    public void takeLastClickedBattleCard(BattleCard battleCard) {
+        if (lastClickedBattleCard != null) {
+            lastClickedBattleCard.getUnselected();
+        }
+        battleCard.getSelected();
+        lastClickedBattleCard = battleCard;
+    }
 
-	public void resetLastClickedBattleCard()
-	{
-		if (lastClickedBattleCard != null) {
-			lastClickedBattleCard.getUnselected();
-			lastClickedBattleCard = null;
-		}
-	}
+    public void resetLastClickedBattleCard() {
+        if (lastClickedBattleCard != null) {
+            lastClickedBattleCard.getUnselected();
+            lastClickedBattleCard = null;
+        }
+    }
 
-	public void changeActivePlayer()
-	{
-		resetLastClickedBattleCard();
-		Zone.RED.deactivate();
-		Zone.GREEN.deactivate();
-		Zone.BLUE.deactivate();
-		started = false;
-		Player nextPlayer = giveOppositePlayerOf(activePlayer);
-		nextPlayer.beginTurn();
-		activePlayer = nextPlayer;
-		updateAllFields();
-	}
+    public void changeActivePlayer() {
+        resetLastClickedBattleCard();
+        Zone.RED.deactivate();
+        Zone.GREEN.deactivate();
+        Zone.BLUE.deactivate();
+        started = false;
+        Player nextPlayer = giveOppositePlayerOf(activePlayer);
+        nextPlayer.beginTurn();
+        activePlayer = nextPlayer;
+        updateAllFields();
+    }
 
-	public void updateAllFields()
-	{
-		ArrayList<Field> allFields = new ArrayList<Field>();
-		allFields.addAll(BATTLEFIELD.giveAllContainingFields());
-		allFields.addAll(PLAYER_1.giveFields());
-		allFields.addAll(PLAYER_2.giveFields());
-		for (Field field : allFields) {
-			field.update();
-		}
-	}
+    public void updateAllFields() {
+        ArrayList<Field> allFields = new ArrayList<Field>();
+        allFields.addAll(BATTLEFIELD.giveAllContainingFields());
+        allFields.addAll(PLAYER_1.giveFields());
+        allFields.addAll(PLAYER_2.giveFields());
+        for (Field field : allFields) {
+            field.update();
+        }
+    }
 
-	/**
-	 * Markiert für den aktuellen Spielzug den Zeitpunkt, ab dem eine anfängliche Veränderung für Farbzone und
-	 * Strömungsrichtung nicht mehr möglich ist. Soll beim ersten Setzen einer Handkarte oder der ersten Aktivierung
-	 * einer Farbzone aufgerufen werden.
-	 */
-	public void startTurn()
-	{
-		if (!started) {
-			started = true;
-		}
-	}
+    /**
+     * Markiert für den aktuellen Spielzug den Zeitpunkt, ab dem eine anfängliche Veränderung für Farbzone und
+     * Strömungsrichtung nicht mehr möglich ist. Soll beim ersten Setzen einer Handkarte oder der ersten Aktivierung
+     * einer Farbzone aufgerufen werden.
+     */
+    public void startTurn() {
+        if (!started) {
+            started = true;
+        }
+    }
 
-	// prüft ob jemande bereits 6 Sektoren hat und gibt den Gewinn in der Konsole aus
-	//TODO: Label hinzufügen
-	public void checkForWin()
-	{
-		int allyCounter  = 0;
-		int enemyCounter = 0;
+    // prüft ob jemande bereits 6 Sektoren hat und gibt den Gewinn in der Konsole aus
+    //TODO: Label hinzufügen
+    public void checkForWin() {
+        int allyCounter = 0;
+        int enemyCounter = 0;
 
-		//prüft die Besitzer der einzelnen Sektoren und zählt
-		for (Sector sector : BATTLEFIELD.giveSectors()) {
-			if (sector.giveCommander().PARTY == Party.ALLY) {
-				allyCounter++;
-			} else {
-				enemyCounter++;
-			}
-		}
+        //prüft die Besitzer der einzelnen Sektoren und zählt
+        for (Sector sector : BATTLEFIELD.giveSectors()) {
+            if (sector.giveCommander().PARTY == Party.ALLY) {
+                allyCounter++;
+            } else {
+                enemyCounter++;
+            }
+        }
 
-		if (allyCounter == 6) {
-			System.out.println("You win");
-		}
-		if (enemyCounter == 6) {
-			System.out.println("You lose");
-		} else {
-			allyCounter = 0;
-			enemyCounter = 0;
-		}
-	}
+        if (allyCounter == 6) {
+            Texture overlay = new Texture("core/assets/visuals/messages/win.png");
+            showWinConditionMessage(overlay);
+
+
+        }
+        if (enemyCounter == 6) {
+            Texture overlay = new Texture("core/assets/visuals/messages/loose.png");
+            showWinConditionMessage(overlay);
+
+        }
+    }
+
+    private void showWinConditionMessage(Texture overlay) {
+
+        overlay.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        Image texture = new Image(overlay);
+        texture.setBounds(0, 0, 1920, 1080);
+        stage.addActor(texture);
+        //TODO Start new Game Button einbinden
+
+    }
 }
