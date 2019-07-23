e0f25ffc6706061da37dd8dfcacd8b22186ac907
Cigdem <cigdem_medigc@yahoo.de>
2019-07-23 19:04:47 +0200

    Image Actor & Methoden angelegt

--- core/src/com/bmeproject/game/bmeProject/screens/battleScreen/battleController/battlefield/sector/EntryField.java
+++ core/src/com/bmeproject/game/bmeProject/screens/battleScreen/battleController/battlefield/sector/EntryField.java
@@ -1,15 +1,12 @@
 package com.bmeproject.game.bmeProject.screens.battleScreen.battleController.battlefield.sector;
 
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
-import com.badlogic.gdx.scenes.scene2d.actions.AlphaAction;
-import com.badlogic.gdx.scenes.scene2d.actions.RemoveAction;
-import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
+import com.badlogic.gdx.scenes.scene2d.actions.*;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.bmeproject.game.bmeProject.screens.battleScreen.BattleController;
 import com.bmeproject.game.bmeProject.screens.battleScreen.battleController.battlefield.Sector;
 import com.bmeproject.game.bmeProject.screens.battleScreen.battleController.FieldUser;
@@ -17,9 +14,10 @@ import com.bmeproject.game.bmeProject.screens.battleScreen.battleController.Batt
 
 public class EntryField extends RingField
 {
+	public final float duration = 0.5f;
 	public final Texture SHOW_SECTOR = new Texture("core/assets/visuals/card.png");
-	public final Sprite SPRITE = new Sprite(SHOW_SECTOR);
-	com.badlogic.gdx.scenes.scene2d.ui.Image image1 = new com.badlogic.gdx.scenes.scene2d.ui.Image(SHOW_SECTOR);
+	Image image1 = new Image(SHOW_SECTOR);
+	RepeatAction repeatAction = new RepeatAction();
 
 
 
@@ -30,12 +28,23 @@ public class EntryField extends RingField
 	public EntryField(FieldUser fieldable, Vector2 position)
 	{
 		super(fieldable, position);
-		//Image image = new Image(SHOW_SECTOR);
+
+
+		float x = getX();
+		float y = getY();
+		int zCor = getZIndex() - 1;
+
+		image1.setPosition(x,y);
+		image1.setColor(1,1,1,0);
+		image1.setZIndex(zCor);
+		System.out.println("Z von bild: "+ zCor + " Z von Field: " + getZIndex());
+		getStage().addActor(image1);
 
 		addListener(new InputListener()
 		{
 			@Override public boolean touchDown(InputEvent event, float x, float y, int pointer, int button)
 			{
+				System.out.println("hi");
 				BattleController battleController = FIELD_USER.giveBattleController();
 				if (battleController.isGoodToGo()) {
 					BattleCard selectedCard = battleController.giveLastClickedBattleCard();
@@ -50,6 +59,7 @@ public class EntryField extends RingField
 				return true;
 			}
 		});
+
 	}
 
 	// ===================================
@@ -69,44 +79,21 @@ public class EntryField extends RingField
 	@Override public void draw(Batch batch, float parentAlpha)
 	{
 		super.draw(batch, parentAlpha);
-		float x = getX();
-		float y = getY();
-
-		BattleController battleController = FIELD_USER.giveBattleController();
-		BattleCard selectedCard = battleController.giveLastClickedBattleCard();
-
-		if(selectedCard != null){
-			if (selectedCard.giveCommander() == FIELD_USER.giveCommander()) {
-			/*	SPRITE.setColor(Color.RED);
-				SPRITE.setPosition(x, y);
-				SPRITE.draw(batch, parentAlpha);*/
-				image1.setPosition(x,y);
-				image1.draw(batch,parentAlpha);
-				update();
-			}
-		}
 	}
 
-	public void update()
-	{
-		SequenceAction sequenceAction = new SequenceAction();
+	public void showBorder(){
+		image1.getActions().clear();
 
-		AlphaAction alphaAction = new AlphaAction();
-		alphaAction.setAlpha(1f);
-		alphaAction.setDuration(0.6f);
-		sequenceAction.addAction(alphaAction);
+		repeatAction.setAction(Actions.sequence(Actions.fadeOut(duration), Actions.fadeIn(duration)));
+		repeatAction.setCount(-1);
 
-		AlphaAction alphaAction2 = new AlphaAction();
-		alphaAction2.setAlpha(0f);
-		alphaAction2.setDuration(0.6f);
-		sequenceAction.addAction(alphaAction2);
-
-		RemoveAction removeAction = new RemoveAction();
-		image1.removeAction(removeAction);
-
-		image1.addAction(sequenceAction);
-		System.out.println("verschwindeeeee");
+		image1.setColor(1,1,1,1);
+		image1.addAction(repeatAction);
 	}
 
+	public void hideBorder(){
+		image1.getActions().clear();
+		image1.setColor(1,1,1,0);
+	}
 
 }
